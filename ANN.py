c0440a23748e80ad2834f760d0fbf02600063d1d
ReemAlattas <ralattas@live.com>
2016-07-30 22:38:50 +0300

    plot correct neuron

--- ANN.py
+++ ANN.py
@@ -9,10 +9,10 @@ def MatrixCreate(rows, cols):
     return matrix
     
 def computePosition(numNeurons):
-    neuronPositions=MatrixCreate(2,numNeurons)
+    neuronPositions=MatrixCreate(2, numNeurons)
     angle = 0.0 
-    angleUpdate = 2 * math.pi /numNeurons 
-    for i in range(0,numNeurons):
+    angleUpdate = 2 * math.pi / numNeurons 
+    for i in range(numNeurons):
         neuronPositions[0,i] = math.sin(angle)
         neuronPositions[1,i] = math.cos(angle)
         angle = angle + angleUpdate
@@ -24,8 +24,10 @@ numNeurons = 10
 neuronValues=MatrixCreate(50,numNeurons)
 for y in range(len(neuronValues[0])):
     neuronValues[0][y] = random.random()
-    
+       
 np = computePosition(numNeurons)
-    
-plt.pyplot.plot(np,'ko',markerfacecolor=[1,1,1], markersize=18)
+print(np) 
+
+plt.pyplot.plot(np[0], np[1], 'ko', markerfacecolor=[1,1,1], markersize=18)
+plt.pyplot.axis([-1, 1, -1, 1])
 plt.pyplot.show()
