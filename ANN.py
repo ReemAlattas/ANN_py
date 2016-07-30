17049e1701e1b965166e2c0272dbe72230bf3c37
ReemAlattas <ralattas@live.com>
2016-07-30 15:10:04 +0300

    plot neuron

--- ANN.py
+++ ANN.py
@@ -9,21 +9,23 @@ def MatrixCreate(rows, cols):
     return matrix
     
 def computePosition(numNeurons):
-    neuronPositions=MatrixCreate(2,10)
+    neuronPositions=MatrixCreate(2,numNeurons)
     angle = 0.0 
     angleUpdate = 2 * math.pi /numNeurons 
     for i in range(0,numNeurons):
-        x = math.sin(angle)
-        y = math.cos(angle)
+        neuronPositions[0,i] = math.sin(angle)
+        neuronPositions[1,i] = math.cos(angle)
         angle = angle + angleUpdate
     
     return neuronPositions
-    
-neuronValues=MatrixCreate(50,10)
+
+numNeurons = 10
+        
+neuronValues=MatrixCreate(50,numNeurons)
 for y in range(len(neuronValues[0])):
     neuronValues[0][y] = random.random()
     
-print(neuronValues)
-
-
-
+np = computePosition(numNeurons)
+    
+plt.pyplot.plot(np,'ko',markerfacecolor=[1,1,1], markersize=18)
+plt.pyplot.show()
